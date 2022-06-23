b20dff4f64be779c7357e9faeb84ecdcba4ef6d5
SaadMd1 <smoujid1@gmail.com>
2022-06-23 16:17:56 +0200

    ...

--- src/main/java/project/crud/CrudApplication.java
+++ src/main/java/project/crud/CrudApplication.java
@@ -3,6 +3,9 @@ package project.crud;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
+import project.crud.models.Client;
+import project.crud.repo.ClientRepo;
+import project.crud.repo.ProduitsRepo;
 
 @SpringBootApplication
 public class CrudApplication {
@@ -11,6 +14,11 @@ public class CrudApplication {
 		ConfigurableApplicationContext configurableApplicationContext =
 				SpringApplication.run(CrudApplication.class, args);
 
+		ClientRepo clientRepository =
+				configurableApplicationContext.getBean(ClientRepo.class);
+
+		ProduitsRepo produitRepository =
+				configurableApplicationContext.getBean(ProduitsRepo.class);
 	}
 
 }
